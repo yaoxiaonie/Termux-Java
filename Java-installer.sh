39ae765eb4e19cc5ac93f0a9fc62dc3c00e3ef72
yaoxiaonie <64189704+yaoxiaonie@users.noreply.github.com>
2020-07-05 04:53:20 +0000

    Update Java-installer.sh

--- Java-installer.sh
+++ Java-installer.sh
@@ -1,7 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 git clone https://github.com/yaoxiaonie/Termux-sudo.git
 cd Termux-sudo
-pkg install ncurses-utils wget
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 777 /data/data/com.termux/files/usr/bin/sudo
 curl -sL https://raw.githubusercontent.com/yaoxiaonie/Termux-java/master/Obtain/Obtain-Resources.sh | sh
