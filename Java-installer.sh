2fbf49104350a430381f1d1a6e2969993ec3bdb0
MistyRain <64189704+yaoxiaonie@users.noreply.github.com>
2020-11-04 23:38:11 +0800

    更新脚本

--- Java-installer.sh
+++ Java-installer.sh
@@ -4,3 +4,4 @@ cd Termux-sudo
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 777 /data/data/com.termux/files/usr/bin/sudo
 curl -sL https://raw.githubusercontent.com/yaoxiaonie/Termux-java/master/Obtain/Obtain-Resources.sh | sh
+proot java -version
