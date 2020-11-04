f61bc0fe669fa0412eb53dbb546d9fb145c6049c
MistyRain <64189704+yaoxiaonie@users.noreply.github.com>
2020-11-05 00:05:15 +0800

    去除jdk版本显示

--- Java-installer.sh
+++ Java-installer.sh
@@ -3,4 +3,3 @@
 curl -sL https://raw.githubusercontent.com/yaoxiaonie/Termux-Java/master/sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 777 /data/data/com.termux/files/usr/bin/sudo
 curl -sL https://raw.githubusercontent.com/yaoxiaonie/Termux-Java/master/Obtain/Obtain-Resources.sh | sh
-proot java -version
